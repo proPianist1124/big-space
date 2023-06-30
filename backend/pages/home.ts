5acb44b6575f3ede50a5e7ac1eb23b20ed13fd85
proPianist1124 <103375096+proPianist1124@users.noreply.github.com>
2023-06-30 04:36:27 +0000

    final utilized for loops for posts

--- backend/pages/home.ts
+++ backend/pages/home.ts
@@ -1,3 +1,5 @@
+// REMEMBER TO ADD THE RATELIMITER BEFORE I SLEEP
+
 const Redis = require("ioredis")
 const db = new Redis(process.env["redis_key"]);
 const colors = require("colors");
@@ -21,7 +23,7 @@ const apiLimiter = rateLimit({
 
 module.exports = function(app) {
 	// login page/home page
-	app.get("/", apiLimiter, function(req, res) {
+	app.get("/", function(req, res) {
 		let postString = [];
 		(async () => {
 			let token = req.cookies.name;
@@ -34,14 +36,15 @@ module.exports = function(app) {
 				// counting method
 				let count = 0;
 				let posts = [];
-				repeatAndCheck();
-				function repeatAndCheck(){
-					let badge = [];
-					(async () => {
-						count+=1;
-						let postId = `p${count}`;
-						if(await db.get(postId) != null){ // if post exists, repeat function
-							// post ui (change as needed)
+
+				if(await db.get("postNumber") == null){
+					await db.set("postNumber", 0);
+					posts = `<br><center><h2 style = "color:#D9544D">no posts available</h2></center>`;
+				}else{
+					for (let i = 1; i < parseInt(await db.get("postNumber")); i++) {
+						let badge = [];
+						(async () => {
+							let postId = `p${i}`;
 							eval(await db.get(postId));
 							eval(await db.get(postString.author));
 							if(user.badge){
@@ -51,20 +54,15 @@ module.exports = function(app) {
 							}
 							let structure = `<a href = "/posts/${postId}"><div class = "postcard" onclick = "document.cookie = 'post=${postId}'"><h2><span style = "color:var(--primary)"><u>${postString.topic}</u>&nbsp;&nbsp;${postString.date}</span>&nbsp;&nbsp;<span style = "color:var(--secondary)">${postString.title}</span></h2><span>${postString.content}</span><br>${postString.image}<p style = "color:var(--tertiary)"><i><img src = "${user.profile}" class = "pfp"/>&nbsp;&nbsp;${user.name}</i>&nbsp;&nbsp;${badge}</p></div></a> ${posts}`;
 							posts = structure; // adds newly evaluated post to continuing string
-							repeatAndCheck();
-						}	else	{
-							if(await db.get("p1") == null){
-								posts = `<br><center><h2 style = "color:#D9544D">no posts available</h2></center>`;
-							}
-							eval(await db.get(token));
-							res.render("home", {
-								posts: posts,
-								user: user.name,
-								profile: user.profile,
-							});
-						}
-					})();
+						})();
+					}
 				}
+				eval(await db.get(token));
+				res.render("home", {
+					posts: posts,
+					user: user.name,
+					profile: user.profile,
+				});
 			}
 		})();
 	});
